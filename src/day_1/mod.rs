2136f8e29da24abdd28235f7806a7884265c9db4
Arthur E. Jones <PartyLich@gmail.com>
2021-04-16 13:39:25 -0500

    feat: add 1-2 solution

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -8,9 +8,10 @@ pub fn read_file(file_path: &str) -> String {
     fs::read_to_string(&path).unwrap_or_else(|why| panic!("couldnt open {}: {}", display, why))
 }
 
+const TARGET: i32 = 2020;
+
 /// find the two entries that sum to 2020 and then multiply those two numbers together
 pub fn one(file_path: &str) -> i32 {
-    const TARGET: i32 = 2020;
     let mut expenses = read_file(file_path)
         .lines()
         .map(|line| line.parse::<i32>().unwrap())
@@ -44,6 +45,44 @@ pub fn one(file_path: &str) -> i32 {
     product
 }
 
+/// find the three entries that sum to 2020 and then multiply those three numbers together
+pub fn two(file_path: &str) -> i32 {
+    let mut expenses = read_file(file_path)
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    // TODO: avoid this sort overhead with a less-bad algo
+    expenses.sort();
+
+    let mut ind = (0, 1, expenses.len() - 1);
+    loop {
+        match expenses[ind.0] + expenses[ind.1] + expenses[ind.2] {
+            sum if sum == TARGET => {
+                let product = expenses[ind.0] * expenses[ind.1] * expenses[ind.2];
+                println!(
+                    "day 1-2\n\ta: {}\n\tb: {}\n\tc: {}\n\tproduct: {}",
+                    expenses[ind.0], expenses[ind.1], expenses[ind.2], product
+                );
+                break product;
+            }
+            sum if sum > TARGET => {
+                ind.2 -= 1;
+            }
+            sum if sum < TARGET => {
+                if (ind.1 - ind.0) > 1 {
+                    // first pointer is a smaller increment, prefer moving it first
+                    ind.0 += 1;
+                } else {
+                    // move second pointer if they are adjacent (ie a smaller increment is not
+                    // possible)
+                    ind.1 += 1;
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -52,6 +91,14 @@ mod tests {
     fn part_one() {
         let expected = 514579;
         let actual = one("./input/1-t.txt");
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn part_two() {
+        let expected = 241861950;
+        let actual = two("./input/1-t.txt");
 
         assert_eq!(actual, expected);
     }
