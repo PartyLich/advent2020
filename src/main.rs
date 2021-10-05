4bacca12952129779fada65664031c81fbce1e5d
Arthur E. Jones <PartyLich@gmail.com>
2021-10-05 16:23:31 -0500

    feat: run 2-1 on actual data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use advent_2020::day_1;
+use advent_2020::*;
 
 fn main() {
     day_1::one("./input/1-1.txt");
     day_1::two("./input/1-1.txt");
+    println!(
+        "Day 2-1:\n\tValid passwords: {}",
+        day_2::one("./input/2-1.txt")
+    );
 }
