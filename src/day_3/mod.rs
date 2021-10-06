fec47f6d935f5d1db62483bf51e646c153c4db30
Arthur E. Jones <PartyLich@gmail.com>
2021-10-06 11:06:38 -0500

    feat: add tree counting fn

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -31,6 +31,48 @@ fn load_terrain(file_path: &str) -> Map {
         .collect()
 }
 
+/// A line slope on a 2 dimensional coordinate system
+#[derive(Debug, Clone, Copy)]
+struct Slope(usize, usize);
+
+impl Slope {
+    pub fn new(x: usize, y: usize) -> Self {
+        // reduce very naively. fine for the specs in the problem, but still deserves better
+        if x > 0 && y % x == 0 {
+            return Self(x / x, y / x);
+        }
+        Self(x, y)
+    }
+}
+
+/// Count the number of [`Tree`](Terrain::Tree) squares encountered on this map by following the
+/// provided slope, starting from the top left (0, 0) square
+fn count_trees(map: &Map, slope: Slope) -> usize {
+    if slope.1 < 1 {
+        panic!(
+            "Supplied slope {:?} cannot take you down the mountain",
+            slope
+        )
+    }
+
+    let mut x = 0;
+    let mut y = 0;
+    let mut count = 0;
+    let map_height = map.len();
+    let map_width = map[0].len();
+
+    while y < map_height {
+        count += match map[y][x] {
+            Terrain::Tree => 1,
+            Terrain::Open => 0,
+        };
+        x = (x + slope.0) % map_width;
+        y += slope.1;
+    }
+
+    count
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -46,4 +88,23 @@ mod test {
         assert_eq!(actual, expected, "{}", msg);
     }
 
+    #[test]
+    fn counts_trees() {
+        let msg = "should count the number of trees for a given slope";
+        let map = vec![
+            vec![Terrain::Open, Terrain::Open, Terrain::Tree, Terrain::Tree],
+            vec![Terrain::Tree, Terrain::Open, Terrain::Open, Terrain::Open],
+        ];
+        let expected = 0;
+        let actual = count_trees(&map, Slope::new(1, 1));
+        assert_eq!(actual, expected, "{}", msg);
+
+        let expected = 1;
+        let actual = count_trees(&map, Slope::new(0, 1));
+        assert_eq!(actual, expected, "{}", msg);
+
+        let expected = 1;
+        let actual = count_trees(&map, Slope::new(4, 1));
+        assert_eq!(actual, expected, "{}", msg);
+    }
 }
