73ed813123060f8516bbf57d3619c379132d6220
Arthur E. Jones <PartyLich@gmail.com>
2021-10-08 10:33:44 -0500

    test: add 5-1 tests

---
+++ src/day_5/mod.rs
@@ -0,0 +1,39 @@
+//! Solutions to 2020 day 5 problems
+//! --- Day 5: Binary Boarding ---
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_pass() {
+        let msg = "should deserialize boarding pass";
+        let actual = "FBFBBFFRLR".parse::<BoardingPass>().unwrap();
+        assert_eq!(actual.seat_id(), 357, "{}", msg);
+        assert_eq!(actual.row, 44, "{}", msg);
+        assert_eq!(actual.col, 5, "{}", msg);
+
+        let actual = "BFFFBBFRRR".parse::<BoardingPass>().unwrap();
+        assert_eq!(actual.seat_id(), 567, "{}", msg);
+        assert_eq!(actual.row, 70, "{}", msg);
+        assert_eq!(actual.col, 7, "{}", msg);
+
+        let actual = "BBFFBBFRLL".parse::<BoardingPass>().unwrap();
+        assert_eq!(actual.seat_id(), 820, "{}", msg);
+        assert_eq!(actual.row, 102, "{}", msg);
+        assert_eq!(actual.col, 4, "{}", msg);
+
+        let actual = "FFFBBBFRRR".parse::<BoardingPass>().unwrap();
+        assert_eq!(actual.seat_id(), 119, "{}", msg);
+        assert_eq!(actual.row, 14, "{}", msg);
+        assert_eq!(actual.col, 7, "{}", msg);
+    }
+
+    #[test]
+    fn part_one() {
+        let msg = "should return the highest seat ID in a list of boarding passes";
+        let expected = 820;
+        let actual = one("input/5-t.txt");
+        assert_eq!(actual, expected, "{}", msg);
+    }
+}
