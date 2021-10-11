73f1c86a86ce49c989ab1c804892c94f6654caca
Arthur E. Jones <PartyLich@gmail.com>
2021-10-11 11:33:36 -0500

    feat: add 9-1 solution

--- src/day_9/mod.rs
+++ src/day_9/mod.rs
@@ -1,13 +1,18 @@
 //! Solutions to 2020 day 6
 //! --- Day 9: Encoding Error ---
+use std::collections::VecDeque;
 use std::num::ParseIntError;
+use std::str::FromStr;
 
 use crate::day_1::read_file;
 
 // reads a newline separated 'series of numbers" (size/range not specified in the problem :( ) from
 // a &str
 fn read_str(serialized: &str) -> Result<Vec<usize>, ParseIntError> {
-    todo!()
+    serialized
+        .lines()
+        .map(FromStr::from_str)
+        .collect::<Result<Vec<_>, _>>()
 }
 
 /// Validate an eXchange-Masking Addition System (XMAS) encoded sequence.
@@ -15,12 +20,36 @@ fn read_str(serialized: &str) -> Result<Vec<usize>, ParseIntError> {
 /// Result is `Ok(())` if the series is valid.
 /// `Err(usize)` contains the first invalid number encountered
 fn validate(preamble_len: usize, series: &[usize]) -> Result<(), usize> {
-    todo!()
+    let mut preamble: VecDeque<_> = series.iter().take(preamble_len).collect();
+
+    // check each value after the preamble
+    for entry in series.iter().skip(preamble_len) {
+        // find two values in the preamble that sum to the current series value
+        let valid = preamble.iter().find(|a| {
+            entry
+                .checked_sub(***a)
+                .map(|target| preamble.contains(&&target))
+                .unwrap_or(false)
+        });
+        if valid.is_none() {
+            return Err(*entry);
+        }
+
+        // valid entry. advance the preamble
+        preamble.push_back(entry);
+        preamble.pop_front();
+    }
+
+    Ok(())
 }
 
 /// find the first number which is not the sum of two of the preamble numbers before it
 pub fn one(file_path: &str) -> usize {
-    todo!()
+    const PREAMBLE: usize = 25;
+    let file_content = read_file(file_path);
+    let series = read_str(&file_content).unwrap();
+
+    validate(PREAMBLE, &series).unwrap_err()
 }
 
 #[cfg(test)]
