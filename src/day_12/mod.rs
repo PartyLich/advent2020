31e328dfe3dba1b57a5400cb11d8352674a3fea1
Arthur E. Jones <PartyLich@gmail.com>
2021-10-13 11:57:07 -0500

    test: add 12-1 test

---
+++ src/day_12/mod.rs
@@ -0,0 +1,21 @@
+//! Solutions to 2020 day 12
+//! --- Day 12: Rain Risk ---
+
+/// return the manhattan distance from the start position
+pub fn one(file_path: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        let msg = "should return the manhattan distance from the start position";
+        let expected = 25;
+        let actual = one("input/12-t.txt");
+        assert_eq!(actual, expected, "{}", msg);
+    }
+    // }
+}
