b38702c71abb520b5d9d023e8a84ef7a709a4371
Arthur E. Jones <PartyLich@gmail.com>
2021-10-15 15:38:46 -0500

    feat: add 14-2 bitmask apply fn

--- src/day_14/two.rs
+++ src/day_14/two.rs
@@ -39,6 +39,38 @@ fn parse_mask(mask_str: &str) -> Mask {
         .collect()
 }
 
+// apply a bit mask to an address and return all resulting addresses
+fn apply_mask(mask: &[MaskOp], address: usize) -> Vec<usize> {
+    const ONE: usize = 1;
+    let value = mask.iter().fold(address, |acc, op| match op {
+        MaskOp::One(offset) => acc | (ONE << offset),
+        _ => acc,
+    });
+
+    // recursive helper function to evaluate b tree branches
+    fn helper(mask: &[MaskOp], address: usize) -> Vec<usize> {
+        mask.iter()
+            .enumerate()
+            .find_map(|(idx, op)| match op {
+                MaskOp::Float(offset) => {
+                    // set all mem locations
+                    let zero = address & !(ONE << offset);
+                    let one = address | (ONE << offset);
+
+                    // walk branches
+                    let mut zeros = helper(&mask[(idx + 1)..], zero);
+                    let mut ones = helper(&mask[(idx + 1)..], one);
+
+                    zeros.append(&mut ones);
+                    Some(zeros)
+                }
+                _ => None,
+            })
+            .unwrap_or_else(|| vec![address])
+    }
+
+    helper(mask, value)
+}
 
 /// returns the sum of the values in memory after executing a the supplied initialization program
 pub fn two(file_path: &str) -> usize {
@@ -48,6 +80,15 @@ pub fn two(file_path: &str) -> usize {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn applies_mask() {
+        let msg = "should return a list of addresses";
+        let mask = parse_mask("000000000000000000000000000000X1001X");
+        let expected = vec![26, 27, 58, 59];
+        let actual = apply_mask(&mask, 42);
+        assert_eq!(actual, expected, "{}", msg);
+    }
 
     #[test]
     fn part_two() {
