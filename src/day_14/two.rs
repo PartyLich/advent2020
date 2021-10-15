95e2784249c51d82b8f1fd4e0e12799f584f8697
Arthur E. Jones <PartyLich@gmail.com>
2021-10-15 15:36:30 -0500

    feat: add 14-2 MaskOp type

--- src/day_14/two.rs
+++ src/day_14/two.rs
@@ -1,6 +1,45 @@
 //! Solutions to 2020 day 14 part two
 //! --- Day 14: Docking Data ---
 
+/// Operation to perform for a single bit of a bitmask
+#[derive(Clone, Debug, PartialEq)]
+enum MaskOp {
+    // set a one bit at the contained offset
+    One(usize),
+    // set all possible values at the contained offset
+    Float(usize),
+}
+
+// parse a [`MaskOp`] from a str
+fn parse_op_str(value: &str, offset: usize) -> Result<MaskOp, String> {
+    let result = match value {
+        "X" => MaskOp::Float(offset),
+        "1" => MaskOp::One(offset),
+        _ => {
+            return Err(format!("Parse failure: invalid character '{}'", value));
+        }
+    };
+
+    Ok(result)
+}
+
+type Mask = Vec<MaskOp>;
+
+// parse a [`Mask`] from a str
+fn parse_mask(mask_str: &str) -> Mask {
+    let mask_len = mask_str.len();
+    mask_str
+        .split("")
+        .enumerate()
+        .filter_map(|(idx, op_str)| {
+            (!op_str.is_empty())
+                .then(|| {})
+                .and_then(|_| parse_op_str(op_str, mask_len - idx).ok())
+        })
+        .collect()
+}
+
+
 /// returns the sum of the values in memory after executing a the supplied initialization program
 pub fn two(file_path: &str) -> usize {
     todo!()
