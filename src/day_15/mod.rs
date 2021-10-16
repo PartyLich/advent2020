e59cc262dc390ce57b655ac2ee5bd0e8e271d654
Arthur E. Jones <PartyLich@gmail.com>
2021-10-15 21:28:29 -0500

    feat: add 15-1 solution

--- src/day_15/mod.rs
+++ src/day_15/mod.rs
@@ -1,8 +1,43 @@
 //! Solutions to 2020 day 15
 //!
+use std::collections::HashMap;
 
-pub fn one(file_path: &str) -> usize {
-    todo!()
+use crate::day_1::read_file;
+use crate::day_13::parse_csv_lossy;
+
+fn step(initial: Vec<u32>, steps: u32) -> u32 {
+    let mut map: HashMap<u32, (u32, u32)> = HashMap::new();
+    let mut last: u32 = 0;
+    initial.iter().enumerate().for_each(|(idx, num)| {
+        let turn = idx as u32 + 1;
+        map.insert(*num, (turn, turn));
+        last = *num;
+    });
+
+    let next = initial.len() as u32 + 1;
+    let mut next_num = 0;
+    for step in next..=steps {
+        let previous_turns = *map.get(&last).unwrap_or(&(step, step));
+        // next number to speak is the difference between the turn number when it was last spoken
+        // and the turn number of the time it was most recently spoken before then
+        next_num = previous_turns.1 - previous_turns.0;
+
+        // update entry for this turn's number
+        let prev = map.get(&next_num).copied().unwrap_or((step, step));
+        map.insert(next_num, (prev.1, step));
+
+        last = next_num;
+    }
+
+    next_num
+}
+
+/// return the 2020th number spoken
+pub fn one(file_path: &str) -> u32 {
+    const STEPS: u32 = 2020;
+    let content = read_file(file_path);
+    let numbers: Vec<u32> = parse_csv_lossy(&content);
+    step(numbers, STEPS)
 }
 
 #[cfg(test)]
