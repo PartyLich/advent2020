859dec82f0b8bf02785ff12679494bd91a2435d4
Arthur E. Jones <PartyLich@gmail.com>
2021-10-19 18:03:44 -0500

    feat: add Expression parser

--- src/day_18/mod.rs
+++ src/day_18/mod.rs
@@ -41,9 +41,90 @@ struct Expression {
     lhs: Box<Operand>,
 }
 
-/// parse an Expression from a string
+// recursive [`Expression`] parsing helper
+fn parse_expr(mut lhs: Option<Operand>, string: &str) -> Result<(&str, Operand), String> {
+    if string.is_empty() {
+        return lhs
+            .map(|lhs| (string, lhs))
+            .ok_or_else(|| "Missing LHS operand".to_string());
+    }
+
+    let mut rhs: Option<Operand> = None;
+    let mut op: Option<Op> = None;
+    for (idx, character) in string.char_indices() {
+        if let (Some(lhs), Some(op), Some(rhs)) = (&lhs, &op, &rhs) {
+            return parse_expr(
+                Some(Operand::Expr(Expression {
+                    lhs: lhs.clone().into(),
+                    rhs: rhs.clone().into(),
+                    op: op.clone(),
+                })),
+                &string[idx..],
+            );
+        }
+
+        match character {
+            ')' => {
+                // end of expression
+                return lhs
+                    .map(|lhs| (&string[idx + 1..], lhs))
+                    .ok_or_else(|| "Missing LHS operand".to_string());
+            }
+            '(' => {
+                // new expression
+                let (remaining, expr) = parse_expr(None, &string[idx + 1..])?;
+
+                if lhs.is_none() {
+                    return parse_expr(Some(expr), remaining);
+                }
+                if rhs.is_none() {
+                    return parse_expr(
+                        Some(Operand::Expr(Expression {
+                            lhs: lhs.unwrap().into(),
+                            rhs: expr.into(),
+                            op: op.unwrap(),
+                        })),
+                        remaining,
+                    );
+                }
+            }
+            character if character.is_digit(10) => {
+                // part of a number
+                // NOTE: input contains only 0-9
+                let num = character.to_digit(10).unwrap();
+                if lhs.is_none() {
+                    lhs = Some(Operand::Number(num));
+                } else if rhs.is_none() {
+                    rhs = Some(Operand::Number(num));
+                }
+            }
+            '*' => {
+                op = Some(Op::Mult);
+            }
+            '+' => {
+                op = Some(Op::Add);
+            }
+            _ => {}
+        }
+    }
+
+    Ok((
+        "",
+        Operand::Expr(Expression {
+            lhs: lhs.unwrap().into(),
+            rhs: rhs.unwrap().into(),
+            op: op.unwrap(),
+        }),
+    ))
+}
+
+/// parse an [`Expression`] from a string
 fn from_str(string: &str) -> Result<Expression, String> {
-    todo!();
+    let (_remaining, expr) = parse_expr(None, string)?;
+    match expr {
+        Operand::Expr(result) => Ok(result),
+        _ => Err("Should be unreachable".to_string()),
+    }
 }
 
 /// Return the result of evaluating an expression
@@ -124,6 +205,31 @@ mod test {
             .into(),
         };
         let actual = evaluate(expression);
+        assert_eq!(actual, expected, "{}", msg);
+    }
+
+    #[test]
+    fn parses() {
+        let msg = "should parse an expression";
+        let expected = Expression {
+            lhs: Operand::Number(1).into(),
+            op: Op::Add,
+            rhs: Operand::Number(3).into(),
+        };
+        let actual = from_str("1 + 3").unwrap();
+        assert_eq!(actual, expected, "{}", msg);
+
+        let rhs = Expression {
+            lhs: Operand::Number(2).into(),
+            rhs: Operand::Number(3).into(),
+            op: Op::Mult,
+        };
+        let expected = Expression {
+            lhs: Operand::Number(1).into(),
+            op: Op::Add,
+            rhs: Operand::Expr(rhs).into(),
+        };
+        let actual = from_str("1 + (2 * 3)").unwrap();
         assert_eq!(actual, expected, "{}", msg);
     }
 
