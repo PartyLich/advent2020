f9186d5fee6946a6cb11034633ad35c3784e404d
Arthur E. Jones <PartyLich@gmail.com>
2021-10-20 18:35:48 -0500

    test: add 19-1 test

---
+++ advent_2020/src/day_19/mod.rs
@@ -0,0 +1,12 @@
+//! Solutions to 2020 day 19 problems
+//! --- Day 19: Monster Messages ---
+
+/// return the count of messages that match rule 0
+pub fn one(file_path: &str) -> usize {
+    todo!();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+}
