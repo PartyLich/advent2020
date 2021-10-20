31ddd7bfa9d9dff029a82b62a3d715fa4a78ecc9
Arthur E. Jones <PartyLich@gmail.com>
2021-10-20 17:19:20 -0500

    feat(parser): add part one content

---
+++ parser/src/lib.rs
@@ -0,0 +1,6 @@
+//! Understanding Parser Combinators
+//!
+//! [Parser combinator article](https://fsharpforfunandprofit.com/posts/understanding-parser-combinators/) implemented in
+//! Rust for...understanding
+//! (un)Organized in the same sequence presented in the articles
+mod one;
