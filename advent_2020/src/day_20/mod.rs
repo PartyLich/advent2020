749c3aae481be8b3ff465e8175d218e1f3dd836d
Arthur E. Jones <PartyLich@gmail.com>
2021-10-23 10:33:33 -0500

    feat: add border comparison fn

--- advent_2020/src/day_20/mod.rs
+++ advent_2020/src/day_20/mod.rs
@@ -1,6 +1,14 @@
 //! Solutions to 2020 day 20
 //! --- Day 20: Jurassic Jigsaw ---
 
+/// return true if two lists match or are mirrors of each other
+fn compare(a: &[char], b: &[char]) -> bool {
+    // match
+    a.iter().zip(b.iter()).all(|(a, b)| a.eq(b)) ||
+        // match flipped
+        a.iter().zip(b.iter().rev()).all(|(a, b)| a.eq(b))
+}
+
 /// returns the product of the four corner tile ids
 pub fn one(file_path: &str) -> usize {
     todo!();
@@ -9,6 +17,21 @@ pub fn one(file_path: &str) -> usize {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn comparison() {
+        let msg = "should return true if lists are equal";
+
+        let a = vec!['.', '.', '.', '#', '#', '#', '.', '#', '.', '.'];
+        let b = vec!['.', '.', '#', '.', '#', '#', '#', '.', '.', '.'];
+        let actual = compare(&a, &b);
+        assert!(actual, "{}", msg);
+
+        let a = vec!['.', '.', '.', '#', '#', '#', '.', '#', '.', '.'];
+        let b = vec!['.', '.', '.', '#', '#', '#', '.', '#', '.', '.'];
+        let actual = compare(&a, &b);
+        assert!(actual, "{}", msg);
+    }
 
     #[test]
     fn part_one() {
