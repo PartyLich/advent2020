29830d2fa728a46539e4bb730d8a529e2f5f4b1c
Arthur E. Jones <PartyLich@gmail.com>
2021-10-30 15:54:17 -0500

    feat: add Tile type

--- advent_2020/src/day_20/two.rs
+++ advent_2020/src/day_20/two.rs
@@ -1,5 +1,27 @@
 //! Solutions to 2020 day 20 part 2
 //! --- Day 20: Jurassic Jigsaw ---
+type TileId = usize;
+type Borders = [Vec<char>; 4];
+type Image = Vec<Vec<char>>;
+
+/// vertical flip, horizontal flip, rotation
+type Orientation = (bool, bool, usize);
+/// id, orientation
+type OrientedTile = (TileId, Orientation);
+
+/// complete image tile
+#[derive(Debug, Clone)]
+struct Tile {
+    /// id
+    id: TileId,
+    /// borders in [top, right, bottom, left] order
+    borders: Borders,
+    /// image excluding borders
+    body: Image,
+    /// adjacent tile ids in [top, right, bottom, left] order
+    neighbors: [Option<OrientedTile>; 4],
+}
+
 
 /// returns count of '#' chars that are not part of a sea monster
 pub fn two(file_path: &str) -> usize {
