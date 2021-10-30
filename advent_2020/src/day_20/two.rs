41f372d977be1ef06001886f5c888536268cd2b8
Arthur E. Jones <PartyLich@gmail.com>
2021-10-30 15:54:52 -0500

    feat: impl FromStr for Tile

--- advent_2020/src/day_20/two.rs
+++ advent_2020/src/day_20/two.rs
@@ -1,5 +1,7 @@
 //! Solutions to 2020 day 20 part 2
 //! --- Day 20: Jurassic Jigsaw ---
+use std::str::FromStr;
+
 type TileId = usize;
 type Borders = [Vec<char>; 4];
 type Image = Vec<Vec<char>>;
@@ -22,6 +24,42 @@ struct Tile {
     neighbors: [Option<OrientedTile>; 4],
 }
 
+impl FromStr for Tile {
+    type Err = &'static str;
+
+    fn from_str(tile_str: &str) -> Result<Self, Self::Err> {
+        let mut lines = tile_str.lines();
+        let head = lines.next().ok_or("Empty input")?;
+        let grid: Vec<_> = lines.map(|line| line.chars().collect::<Vec<_>>()).collect();
+
+        let (_, head) = head.split_once(" ").ok_or("Failed to parse header")?;
+        let (head, _) = head.split_once(":").ok_or("Failed to parse header")?;
+        let id = head.parse().map_err(|_e| "Failed to parse tile ID")?;
+
+        // get borders (clockwise) and body
+        let back = grid.len() - 1;
+        let cols = grid[0].len();
+        let top = grid[0].to_owned();
+        let bottom = grid[back].iter().copied().rev().collect::<Vec<_>>();
+        let mut left = vec![];
+        let mut right = vec![];
+        let mut body = vec![];
+        for r in 0..grid.len() {
+            right.push(grid[r][cols - 1]);
+            left.push(grid[back - r][0]);
+            if (1..grid.len() - 1).contains(&r) {
+                body.push(grid[r][1..(cols - 1)].to_owned());
+            }
+        }
+
+        Ok(Self {
+            id,
+            borders: [top, right, bottom, left],
+            body,
+            neighbors: [None; 4],
+        })
+    }
+}
 
 /// returns count of '#' chars that are not part of a sea monster
 pub fn two(file_path: &str) -> usize {
