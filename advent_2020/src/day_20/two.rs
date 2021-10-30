fa5b4530fd1fd5d197d5f62a8255f40fed9605f7
Arthur E. Jones <PartyLich@gmail.com>
2021-10-30 16:20:29 -0500

    feat: assemble single row of tiles

--- advent_2020/src/day_20/two.rs
+++ advent_2020/src/day_20/two.rs
@@ -190,11 +190,89 @@ fn find_neighbors(mut tiles: Vec<Tile>) -> (Option<TileId>, HashMap<TileId, Tile
     (top_left, result)
 }
 
+/// return the oriented tile on the provided side with the current tile oriented
+fn get_side(
+    neighbors: &[Option<OrientedTile>; 4],
+    vert: bool,
+    horz: bool,
+    rotation: usize,
+    side: usize,
+) -> Option<OrientedTile> {
+    todo!()
+}
+
+fn rotate_grid<T: Clone>(grid: Vec<Vec<T>>, units: usize) -> Vec<Vec<T>> {
+    todo!()
+}
+
+fn next_orient(
+    (mut glob_vert, mut glob_horz, absolute_rotation): Orientation,
+    (mut vert, mut horz, rot): Orientation,
+) -> Orientation {
+    todo!()
+}
+
+/// return a complete row of tiles starting from the leftmost oriented tile
 fn get_row(
     tile_map: &mut HashMap<TileId, Tile>,
     top_left: OrientedTile,
 ) -> (Option<OrientedTile>, Image) {
-    todo!()
+    let Tile {
+        mut neighbors,
+        body,
+        ..
+    } = tile_map.remove(&top_left.0).unwrap();
+
+    // NOTE: square tiles only
+    let height = body.len();
+    let mut glob_vert = top_left.1 .0;
+    let mut glob_horz = top_left.1 .1;
+    let mut absolute_rotation = top_left.1 .2;
+
+    // save id of bottom neighbor
+    // need the next row to be oriented properly (relative to previous row). same sort of
+    // combination that happens below
+    let next_row = get_side(&neighbors, glob_vert, glob_horz, absolute_rotation, 2).map(
+        |(next_id, next_orientation)| {
+            let orientation = next_orient(top_left.1, next_orientation);
+            (next_id, orientation)
+        },
+    );
+
+    // orient body
+    let mut row = rotate_grid(body, absolute_rotation);
+
+    while let Some((next_id, next_o)) =
+        get_side(&neighbors, glob_vert, glob_horz, absolute_rotation, 1)
+    {
+        let next_tile = tile_map.remove(&next_id).unwrap();
+
+        let (v, h, r) = next_orient((glob_vert, glob_horz, absolute_rotation), next_o);
+        absolute_rotation = r;
+        glob_vert = v;
+        glob_horz = h;
+
+        let next_body = rotate_grid(next_tile.body, absolute_rotation);
+
+        for (idx, line) in row.iter_mut().enumerate() {
+            let mut r = idx;
+            // vert flip
+            if glob_vert {
+                r = (height - 1) - idx;
+            }
+
+            // horz flip
+            if glob_horz {
+                line.extend(next_body[r].iter().rev());
+            } else {
+                line.extend(next_body[r].iter());
+            }
+        }
+
+        neighbors = next_tile.neighbors;
+    }
+
+    (next_row, row)
 }
 
 /// assemble tiles into an image
