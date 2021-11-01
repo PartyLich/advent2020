db847da6e25eab3f20b144667473b3803a886011
Arthur E. Jones <PartyLich@gmail.com>
2021-11-01 14:13:07 -0600

    feat(parser): impl Display for Parser

--- parser/src/three.rs
+++ parser/src/three.rs
@@ -388,6 +388,12 @@ pub mod three {
         }
     }
 
+    impl<O> std::fmt::Display for Parser<'_, O> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Parser {}", self.label)
+        }
+    }
+
     #[cfg(test)]
     mod test {
         use super::*;
