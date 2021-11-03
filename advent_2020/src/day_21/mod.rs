cf110f9abe66697bcccaf8dac09f8520d6f17f93
Arthur E. Jones <PartyLich@gmail.com>
2021-11-03 11:07:04 -0600

    feat: add Ingredient type

--- advent_2020/src/day_21/mod.rs
+++ advent_2020/src/day_21/mod.rs
@@ -1,5 +1,17 @@
 //! Solutions to 2020 day 21 problems
 //! --- Day 21: Allergen Assessment ---
+use std::collections::HashMap;
+
+/// a food ingredient
+#[derive(Debug, Default)]
+struct Ingredient<'a> {
+    /// number of foods this ingredient appears in
+    appearances: usize,
+    /// max times an allergen is associated with this ingredient
+    max: (&'a str, usize),
+    /// map of allergens associated with this ingredient
+    allergens: HashMap<&'a str, usize>,
+}
 
 /// return count of allergen free ingredient appearances
 pub fn one(file_path: &str) -> usize {
