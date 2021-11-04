8b8499c3ed9dc048f8f712124d6a0bef4394be9c
Arthur E. Jones <PartyLich@gmail.com>
2021-11-04 13:08:17 -0600

    feat: add step function

--- advent_2020/src/day_23/mod.rs
+++ advent_2020/src/day_23/mod.rs
@@ -1,5 +1,6 @@
 //! Solutions to 2020 day 23 problems
 //! --- Day 23: Crab Cups ---
+type State = Vec<usize>;
 
 /// parse cup state from str
 fn parse(input: &str) -> Result<Vec<usize>, String> {
@@ -14,6 +15,45 @@ fn parse(input: &str) -> Result<Vec<usize>, String> {
         .collect()
 }
 
+/// execute a single step of the game and return the new state
+///
+/// Next cup is always at index 0
+fn step(mut state: State) -> State {
+    let current = 0;
+    let removed: Vec<_> = state.splice((current + 1)..(current + 4), []).collect();
+
+    // destination cup: the cup with a label equal to the current cup's label minus one.
+    // If the destination label is not in the current set,
+    //   - subtract one until a label is found OR
+    //   - if the value goes below the lowest value on any cup's label, select the highest value label
+    //     instead.
+    let mut dest_label = state[current] - 1;
+    let mut dest = None;
+    while dest_label > 0 && dest == None {
+        dest = state
+            .iter()
+            .enumerate()
+            .find(|(_, label)| **label == dest_label)
+            .map(|(idx, _)| idx);
+        dest_label -= 1;
+    }
+
+    if dest.is_none() {
+        dest = state
+            .iter()
+            .enumerate()
+            .max_by(|a, b| a.1.cmp(b.1))
+            .map(|(idx, _)| idx);
+    }
+    let dest = dest.unwrap();
+    state.splice(dest + 1..dest + 1, removed);
+
+    // select a new current cup: the cup which is immediately clockwise of the current cup.
+    state.rotate_left(1);
+
+    state
+}
+
 /// should return the ordered cup labels after cup 1 following 100 steps
 pub fn one(file_path: &str) -> String {
     todo!();
@@ -22,6 +62,24 @@ pub fn one(file_path: &str) -> String {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn steps() {
+        let msg = "should return the ordered cup labels after a step";
+        let initial = vec![3, 8, 9, 1, 2, 5, 4, 6, 7];
+        let expected = vec![2, 8, 9, 1, 5, 4, 6, 7, 3];
+        let actual = step(initial);
+        assert_eq!(actual, expected, "{}", msg);
+
+        let msg = "should return the ordered cup labels after 10 steps";
+        let initial = vec![3, 8, 9, 1, 2, 5, 4, 6, 7];
+        let expected = vec![8, 3, 7, 4, 1, 9, 2, 6, 5];
+        let mut actual = initial;
+        for _ in 0..10 {
+            actual = step(actual);
+        }
+        assert_eq!(actual, expected, "{}", msg);
+    }
 
     #[test]
     fn part_one() {
