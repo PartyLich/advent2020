e34a0087fbae9173c5929795475b146334555f51
Arthur E. Jones <PartyLich@gmail.com>
2021-11-04 10:38:07 -0600

    feat: add 22-2 Game type

--- advent_2020/src/day_22/two.rs
+++ advent_2020/src/day_22/two.rs
@@ -1,5 +1,33 @@
 //! Solutions to 2020 day 22 problems part two
 //! --- Day 22: Crab Combat ---
+use std::collections::HashSet;
+
+use super::Deck;
+
+/// Completed game state, as a (winner index, winning deck) pair
+type GameResult = (usize, Deck);
+
+/// Recursive Combat game state
+#[derive(Debug)]
+enum Game {
+    /// Game with rounds remaining to be played
+    InProgress {
+        decks: (Deck, Deck),
+        previous_decks: (HashSet<Deck>, HashSet<Deck>),
+    },
+    /// Game that has been won
+    Complete(GameResult),
+}
+
+impl Game {
+    /// create a new game of Combat
+    pub fn new(deck1: Deck, deck2: Deck) -> Self {
+        Self::InProgress {
+            decks: (deck1, deck2),
+            previous_decks: Default::default(),
+        }
+    }
+}
 
 /// returns the winning score from a game of 'Combat'
 pub fn two(file_path: &str) -> usize {
