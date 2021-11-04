a4203d962cd04a464c7f5d8e94353b49d56e584c
Arthur E. Jones <PartyLich@gmail.com>
2021-11-04 12:35:28 -0600

    feat: parse state from input str

--- advent_2020/src/day_23/mod.rs
+++ advent_2020/src/day_23/mod.rs
@@ -1,6 +1,19 @@
 //! Solutions to 2020 day 23 problems
 //! --- Day 23: Crab Cups ---
 
+/// parse cup state from str
+fn parse(input: &str) -> Result<Vec<usize>, String> {
+    input
+        .trim()
+        .chars()
+        .map(|ch| {
+            ch.to_digit(10)
+                .map(|d| d as usize)
+                .ok_or(format!("Failed to parse digit {}", ch))
+        })
+        .collect()
+}
+
 /// should return the ordered cup labels after cup 1 following 100 steps
 pub fn one(file_path: &str) -> String {
     todo!();
