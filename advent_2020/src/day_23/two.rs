d57c56de27a9b04e8ef5bafc508362123587eb7b
Arthur E. Jones <PartyLich@gmail.com>
2021-11-05 11:52:40 -0600

    feat: add 23-3 input parsing

--- advent_2020/src/day_23/two.rs
+++ advent_2020/src/day_23/two.rs
@@ -1,7 +1,25 @@
 //! Solutions to 2020 day 23 problems part 2
 //! --- Day 23: Crab Cups ---
+use std::collections::HashMap;
 
 use super::*;
+
+// we care about the indexes of items that have been acted on, but don't want to store or
+// manipulate anything that isnt touched
+type Cups = HashMap<usize, usize>;
+
+/// parse cup state from str
+fn parse(input: &str) -> Result<Cups, String> {
+    input
+        .trim()
+        .char_indices()
+        .map(|(idx, ch)| {
+            ch.to_digit(10)
+                .map(|d| (d as usize, idx))
+                .ok_or(format!("Failed to parse digit {}", ch))
+        })
+        .collect()
+}
 
 /// returns the product of the two cup labels immediately clockwise of cup 1 after ten million
 /// steps
