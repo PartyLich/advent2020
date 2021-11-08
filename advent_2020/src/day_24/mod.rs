dd0481fbf247cf604b85278e4a84607c92174077
Arthur E. Jones <PartyLich@gmail.com>
2021-11-08 15:56:15 -0600

    feat: parse Direction from str

--- advent_2020/src/day_24/mod.rs
+++ advent_2020/src/day_24/mod.rs
@@ -2,6 +2,9 @@
 //! --- Day 24: Lobby Layout ---
 use std::collections::HashMap;
 use std::ops::Add;
+use std::str::FromStr;
+
+use parser::three::three::{choice, one_or_more, p_char};
 
 use crate::day_1::read_file;
 
@@ -14,6 +17,29 @@ impl Add for Direction {
 
     fn add(self, other: Self) -> Self {
         Self(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl FromStr for Direction {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let w = p_char('w').map(|_| Direction(-1, 0));
+        let nw = p_char('n').and_then(w.clone()).map(|_| Direction(0, -1));
+        let sw = p_char('s').and_then(w.clone()).map(|_| Direction(-1, 1));
+        let e = p_char('e').map(|_| Direction(1, 0));
+        let ne = p_char('n').and_then(e.clone()).map(|_| Direction(1, -1));
+        let se = p_char('s').and_then(e.clone()).map(|_| Direction(0, 1));
+        let all = choice([nw, ne, sw, se, w, e]);
+
+        let parser = one_or_more(all).with_label("Instruction List".to_string());
+
+        match parser.parse(s) {
+            Ok((_input, value)) => Ok(value
+                .into_iter()
+                .fold(Direction(0, 0), |acc, next| acc + next)),
+            Err(err) => Err(format!("{}", err)),
+        }
     }
 }
 
@@ -40,6 +66,18 @@ pub fn one(file_path: &str) -> usize {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn parsing() {
+        let msg = "should parse a tile flip instruction";
+        let expected = Direction(0, 0);
+        let actual: Direction = "nwwswee".parse().unwrap();
+        assert_eq!(actual, expected, "{}", msg);
+
+        let expected = Direction(-3, 2);
+        let actual: Direction = "sesenwnenenewseeswwswswwnenewsewsw".parse().unwrap();
+        assert_eq!(actual, expected, "{}", msg);
+    }
 
     #[test]
     fn part_one() {
