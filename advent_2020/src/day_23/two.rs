cd2acc0a92db849022babd7de03e32f4aadce231
Arthur E. Jones <PartyLich@gmail.com>
2021-11-08 13:25:27 -0600

    feat: add step fn

--- advent_2020/src/day_23/two.rs
+++ advent_2020/src/day_23/two.rs
@@ -54,6 +54,88 @@ fn rotate_left(map: &mut Cups, units: usize, len: usize) {
     }
 }
 
+/// execute a single step of the game and return the new state
+///
+/// Next cup is always at index 0
+fn step((mut state, current_label, mut max, mut shifts): State) -> State {
+    let current_idx = *state.get(&current_label).unwrap_or(&(current_label - 1));
+
+    if current_idx > 0 {
+        rotate_left(&mut state, current_idx, 1_000_000);
+        shifts += current_idx;
+    }
+
+    let mut removed: Vec<_> = state
+        .iter()
+        .filter(|(_label, idx)| (current_idx + 1..current_idx + 4).contains(idx))
+        .map(|(label, _idx)| *label)
+        .collect();
+    while removed.len() < 3 {
+        max += 1;
+        let index = removed.len() + 1;
+        state.insert(max, index);
+        removed.push(max);
+    }
+
+    let remaining = state
+        .iter()
+        .filter(|(_label, idx)| !(current_idx + 1..current_idx + 4).contains(idx));
+
+    // destination cup: the cup with a label equal to the current cup's label minus one.
+    // If the destination label is not in the current set,
+    //   - subtract one until a label is found OR
+    //   - if the value goes below the lowest value on any cup's label, select the highest value label
+    //     instead.
+    let mut dest_label = current_label - 1;
+    let mut dest = None;
+    while dest_label > 0 && dest == None {
+        dest = remaining
+            .clone()
+            .find(|(label, _idx)| **label == dest_label)
+            .map(|(_label, idx)| idx);
+        dest_label -= 1;
+    }
+
+    let dest = if dest.is_none() {
+        let mut max_dest = 999_999 - shifts;
+        while removed.contains(&&max_dest) {
+            max_dest -= 1;
+        }
+        max_dest
+    } else {
+        *dest.unwrap()
+    };
+
+    // update indices somehow
+    for (label, idx) in state.iter_mut() {
+        if removed.contains(&label) {
+            // insert
+            *idx += dest - 3;
+        } else {
+            // shift
+            if (4..=dest).contains(idx) {
+                *idx -= 3;
+            }
+        }
+    }
+
+    rotate_left(&mut state, 1, 1_000_000);
+    shifts += 1;
+
+    // find next current cup
+    let next = state
+        .iter()
+        .find_map(|(label, idx)| if *idx == 0 { Some(*label) } else { None })
+        .unwrap_or_else(|| {
+            max += 1;
+            // insert
+            state.insert(max, 0);
+            max
+        });
+
+    (state, next, max, shifts)
+}
+
 /// returns the product of the two cup labels immediately clockwise of cup 1 after ten million
 /// steps
 pub fn two(file_path: &str) -> usize {
